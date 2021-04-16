c12579cccedac77bd3e5d2a6779a789d5ca299c1
Jon Gibler <jgibler@gmail.com>
2021-04-15 19:34:36 -0500

    API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <div>Hola Monterrey</div>
+  <div>{{textFromApi}}</div>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
+
+
     return {
-      value: "World"
+      textFromApi: ''
     };
+    
+  },
+  async mounted() {
+    
+    let response = await( await fetch(getApiUrl() + 'welcome')).json();
+    this.textFromApi = response.text;
+
   }
 };
+
+function getApiUrl() {
+  let requestUrl = "";
+  if (location.href.startsWith("http://localhost")) {
+    requestUrl = "http://localhost:7071/api/";
+  } else {
+    requestUrl = "/api/";
+  }
+  return requestUrl;
+}	
 </script>
